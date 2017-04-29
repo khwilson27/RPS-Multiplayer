955f32b5848623f59e8ead3a8d68f27c8049f216
Kevin Wilson <Kevin Wilson>
2017-04-28 17:05:37 -0700

    first push, index, css, images, js w/ firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyAPDQyy4hH2Q-6E0INLR4Dz_hkXmlz2nn0",
+	    authDomain: "rps-multiplayer-8e4af.firebaseapp.com",
+	    databaseURL: "https://rps-multiplayer-8e4af.firebaseio.com",
+	    projectId: "rps-multiplayer-8e4af",
+	    storageBucket: "rps-multiplayer-8e4af.appspot.com",
+	    messagingSenderId: "485695189412"
+	  };
+	  firebase.initializeApp(config);
+
+    
+
+}) //closing document.ready
