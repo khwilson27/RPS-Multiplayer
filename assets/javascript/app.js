f6fbe69134b4f4a40d7e8d53b9b8a04ce90b8fa3
Kevin Wilson <Kevin Wilson>
2017-04-28 19:01:19 -0700

    slight changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,40 @@ $(document).ready(function() {
 	  };
 	  firebase.initializeApp(config);
 
+
+
+	  // Create a variable to reference the database.
+	  var db = firebase.database();
+
+	  db.ref().set({
+	    player1name: "",
+	    player1choice: "",
+	    player1wins: 0,
+	    player1losses: 0,
+	    player2name: "",
+	    player2choice: "",
+	    player2wins: 0,
+	    player2losses: 0,
+	  });
+
+	  $("#mainContent").on("click", ".clickableImg", function(){
+
+	  	var choice = $(this).data("choice");
+	  	var name = $(".nameInput").val();
+
+	  	if (!name) {
+	  		alert("Please input and submit a valid name!");
+	  	} 
+		else {
+	  		$(".clickableImg").removeClass("clickableImg");
+	  	}
+
+
+
+
+
+	  })
+
     
 
 }) //closing document.ready
